73c2dba4a7cb5b3596bf041e4827e486d282b1cd
Muhamad Jamil Firdaus <muhamadjamil779@gmail.com>
2023-05-17 11:53:25 +0700

    extension method

---
+++ bin/Learn Oop/4.Extension_Method.dart
@@ -0,0 +1,46 @@
+class Person {
+
+  String name = "Guest";
+  String? address;
+  final String country = 'Indonesia';
+
+  void sayHello(String paramName) {
+    print('Hello $paramName, My Name Is $name');
+  }
+
+  void hello() {
+    print('Hello My Name Is $name');
+  }
+
+  String getName(){
+    return 'Hello My Name Is $name';
+  }
+}
+  // Method Extension
+    extension sayGoodByeOnPerson on Person {
+      void sayGoodBye(String paramName) {
+        print('Goob By $paramName, on $name');
+    }
+
+}
+
+void main() {
+  var person1 = new Person();
+
+  /* Manipulation Field
+    ====== */
+  person1.name = "Muhamad Jamil Firdaus";
+  person1.address = "Bandung";
+  // person1.country = 'Saudi Arabia'; tidak bisa mengubah final field
+  print(person1.name);
+  print(person1.address);
+  print(person1.country);
+
+  // call method
+  person1.sayHello('Muhamad Jamil Firdaus');
+  person1.hello();
+
+  // Call Method Extension
+  person1.sayGoodBye('Bima');
+
+}
