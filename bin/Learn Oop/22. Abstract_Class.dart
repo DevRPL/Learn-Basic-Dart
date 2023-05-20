b34b01355e404af5f5e30868a640c24726225958
Muhamad Jamil Firdaus <muhamadjamil779@gmail.com>
2023-05-20 09:39:45 +0700

    abstact class

---
+++ bin/Learn Oop/22. Abstract_Class.dart
@@ -0,0 +1,7 @@
+import '../data/Location.dart';
+
+void main() {
+  var city = City('Bandung');
+  print(city);
+}
+
--- bin/Learn Oop/Import.dart
+++
@@ -1,7 +0,0 @@
-import '../data/Category.dart';
-
-void main() {
-  var category = Category('1', 'Laptop');
-  print(category.id);
-  print(category.name);
-}
