1a53ac5dd8f0974c7570e38e12631c754509682b
Muhamad Jamil Firdaus <muhamadjamil779@gmail.com>
2023-05-22 10:07:31 +0700

    25 getter & setter

---
+++ bin/Learn Oop/25. Getter_Setter.dart
@@ -0,0 +1,11 @@
+import '../data/Rectangle.dart';
+
+void main() {
+    var rectangle = Rectangle();
+
+    rectangle.width = 100;
+    print(rectangle.width);
+
+    rectangle.length = 120;
+    print(rectangle.length);
+}
