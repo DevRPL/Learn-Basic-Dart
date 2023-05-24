aaea19896d709f796c6d7567ebc091f5955bcefa
Muhamad Jamil Firdaus <muhamadjamil779@gmail.com>
2023-05-24 11:09:12 +0700

    callable class

---
+++ bin/Learn Oop/31. Callable Class.dart
@@ -0,0 +1,6 @@
+import '../data/Sum.dart';
+
+void main() {
+   var sum =  Sum(120,10);
+    print(sum());
+}
---
+++ bin/data/Sum.dart
@@ -0,0 +1,9 @@
+class Sum {
+  int first,second;
+
+  Sum(this.first, this.second);
+
+  int call() {
+    return first + second;
+  }
+}
