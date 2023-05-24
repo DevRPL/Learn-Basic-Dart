8d8a675d97bebfd59abe66b9d5c709e4f718f4d5
Muhamad Jamil Firdaus <muhamadjamil779@gmail.com>
2023-05-24 23:03:55 +0700

    handle error Exception

---
+++ bin/data/Exception.dart
@@ -0,0 +1,19 @@
+class ValidationException implements Exception {
+
+  String message;
+  ValidationException(this.message);
+}
+
+class Validation {
+  static void validate(String username, String password) {
+    if(username == '') {
+      throw ValidationException('Username is Blank');
+    } else if (password == '') {
+      throw ValidationException('Password is Blank');
+    }
+  }
+}
+
+void main() {
+  Validation.validate('', '');
+}
